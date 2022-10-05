1fe2402c031280287897db9ed43782584ca6c9f0
KeysGilbert <gbk5070@psu.edu>
2022-10-05 14:02:30 -0400

    begin styling (text as placeholder)

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,7 +4,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[],
+      children: <Widget>[
+        Expanded(
+            flex: 4,
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Color(0xffff4d4d),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0))),
+              child: Text('just a placeholder',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0)),
+            )),
+        Expanded(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.center,
+              color: Colors.white,
+              child: ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.lightBlueAccent)),
+                child: Text(
+                  'SPIN',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ))
+      ],
     );
   }
 }
