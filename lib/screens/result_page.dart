f588bd6c123e014d4365c82f97940ee43c7c2a9b
KeysGilbert <gbk5070@psu.edu>
2022-10-19 14:04:07 -0400

    display data from API, will update

---
+++ lib/screens/result_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:randate/model/date.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({Key? key}) : super(key: key);
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<Date>(context, listen: false).getActivityData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(Provider.of<Date>(context).activityData['activity']),
+    );
+  }
+}
