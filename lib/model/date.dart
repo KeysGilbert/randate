7b332447d18a7956cb99a6c691f85f0b34d0bef9
KeysGilbert <gbk5070@psu.edu>
2022-11-08 14:17:09 -0500

    add actvity from GET request to list

--- lib/model/date.dart
+++ lib/model/date.dart
@@ -8,7 +8,7 @@ class Date extends ChangeNotifier {
       "https://www.boredapi.com/api/activity?type=recreational&participants=1";
 
   late var _activityData;
-  late List<dynamic> dateList = [];
+  List<dynamic> dateList = [];
 
   Future getData() async {
     var uri = Uri.parse(_url);
@@ -19,10 +19,9 @@ class Date extends ChangeNotifier {
     //check if successful
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
-      
       return decodedData;
     } else {
-      throw Exception("Could not retrive data.");
+      throw Exception("Could not retrieve data.");
     }
   }
 
@@ -30,9 +29,9 @@ class Date extends ChangeNotifier {
   void awaitActivityData() async {
     //await for decoded data
     _activityData = await getData();
-
-    //store in list for use in ViewPage
-    dateList = _activityData['activity'] as List;
+  
+    //add activity to list
+    dateList.add(_activityData['activity']);
     notifyListeners();
   }
 
