c1ed845e6ea1ca7444d3b062d110c154cb10d2bb
KeysGilbert <gbk5070@psu.edu>
2022-11-21 16:54:15 -0500

    add date and show snackbar

--- lib/screens/add_date_page.dart
+++ lib/screens/add_date_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:randate/model/date.dart';
 
 class AddDatePage extends StatelessWidget {
   final _controller = TextEditingController();
+  late String newDate;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +26,10 @@ class AddDatePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 20),
             ),
+            SizedBox(
+              width: 15.0,
+              height: 15.0,
+            ),
             TextField(
               controller: _controller,
               autofocus: true,
@@ -30,7 +37,31 @@ class AddDatePage extends StatelessWidget {
               decoration: InputDecoration(
                   border: UnderlineInputBorder(),
                   hintText: "What shall we do?"),
-            )
+              onChanged: (value) {
+                newDate = value; //store inputted text
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  _controller.clear();
+
+                  //add input to list of dates
+                  Provider.of<Date>(context, listen: false)
+                      .dateList
+                      .add(newDate);
+
+                  //show snackbar notifying that date was added
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      duration: Duration(seconds: 2),
+                      backgroundColor: Colors.black12,
+                      content: Text("Date added!")));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.lightBlueAccent,
+                ),
+                child: Text("Add",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white)))
           ],
         ),
       ),
