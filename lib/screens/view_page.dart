916e446bf2702836cdcea77fbada5bebe2900273
KeysGilbert <gbk5070@psu.edu>
2022-12-01 17:39:54 -0500

    setup AnimationController

--- lib/screens/view_page.dart
+++ lib/screens/view_page.dart
@@ -7,13 +7,26 @@ class ViewPage extends StatefulWidget {
   State<ViewPage> createState() => _ViewPageState();
 }
 
-class _ViewPageState extends State<ViewPage> {
+class _ViewPageState extends State<ViewPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+ 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
+    //setup controller for animations upon initialization
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
 
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
   }
 
   @override
@@ -24,18 +37,15 @@ class _ViewPageState extends State<ViewPage> {
             shrinkWrap: true,
             itemCount: date.dateList.length,
             itemBuilder: (context, index) {
-              
               return Dismissible(
                   key: UniqueKey(),
                   direction: DismissDirection.horizontal,
                   onDismissed: (direction) {
                     date.dateList.removeAt(index);
                   },
-                  child: ListTile(
-                    title: Text(date.dateList[index])
-                  ));
+                  child: ListTile(title: Text(date.dateList[index])));
             }),
-      ); 
+      );
     });
   }
 }
