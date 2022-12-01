dee676f45ab84ed97f3e34a7d77180ea2a42fdb2
KeysGilbert <gbk5070@psu.edu>
2022-12-01 18:26:22 -0500

    add SingleChildScrollView to stop column overflow

--- lib/screens/add_date_page.dart
+++ lib/screens/add_date_page.dart
@@ -16,55 +16,59 @@ class AddDatePage extends StatelessWidget {
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(15.0),
                 topRight: Radius.circular(15.0))),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Add Date",
-              style: TextStyle(
-                  color: Colors.lightBlueAccent,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
-            ),
-            SizedBox(
-              width: 15.0,
-              height: 15.0,
-            ),
-            TextField(
-              controller: _controller,
-              autofocus: true,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  hintText: "What shall we do?"),
-              onChanged: (value) {
-                newDate = value; //store inputted text
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  _controller.clear();
-
-                  //add input to list of dates
-                  Provider.of<Date>(context, listen: false)
-                      .dateList
-                      .add(newDate);
-
-                  //show snackbar notifying that date was added
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      duration: Duration(seconds: 2),
-                      backgroundColor: Colors.black12,
-                      content: Text("Date added!")));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.lightBlueAccent,
+        
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Add Date",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 180, 207, 219),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
                 ),
-                child: Text("Add",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white)))
-          ],
+                SizedBox(
+                  width: 15.0,
+                  height: 15.0,
+                ),
+                TextField(
+                  controller: _controller,
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: "What shall we do?"),
+                  onChanged: (value) {
+                    newDate = value; //store inputted text
+                  },
+                ),
+                TextButton(
+                    onPressed: () {
+                      _controller.clear();
+                  
+                      //add input to list of dates
+                      Provider.of<Date>(context, listen: false)
+                          .dateList
+                          .add(newDate);
+                  
+                      //show snackbar notifying that date was added
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          duration: Duration(seconds: 2),
+                          backgroundColor: Colors.black12,
+                          content: Text("Date added!")));
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                    child: Text("Add",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white)))
+              ],
+            ),
+          ),
         ),
-      ),
+      
     );
   }
 }
