c16d5e6cba9f115c781a9ddf753ae6b2eb0dfe5a
KeysGilbert <gbk5070@psu.edu>
2022-12-02 12:50:15 -0500

    implement slide transition

--- lib/screens/view_page.dart
+++ lib/screens/view_page.dart
@@ -17,7 +17,7 @@ class _ViewPageState extends State<ViewPage> with SingleTickerProviderStateMixin
 
     //setup controller for animations upon initialization
     _controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 1));
+        AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
 
     _controller.forward();
   }
@@ -37,13 +37,19 @@ class _ViewPageState extends State<ViewPage> with SingleTickerProviderStateMixin
             shrinkWrap: true,
             itemCount: date.dateList.length,
             itemBuilder: (context, index) {
-              return Dismissible(
-                  key: UniqueKey(),
-                  direction: DismissDirection.horizontal,
-                  onDismissed: (direction) {
-                    date.dateList.removeAt(index);
-                  },
-                  child: ListTile(title: Text(date.dateList[index])));
+              return SlideTransition(
+                position: Tween<Offset>(
+                    begin: Offset(1,0),
+                    end: Offset(0,0)
+                ).animate(CurvedAnimation(parent: _controller, curve: Curves.bounceIn)),
+                child: Dismissible(
+                    key: UniqueKey(),
+                    direction: DismissDirection.horizontal,
+                    onDismissed: (direction) {
+                      date.dateList.removeAt(index);
+                    },
+                    child: ListTile(title: Text(date.dateList[index]))),
+              );
             }),
       );
     });
